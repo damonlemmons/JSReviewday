87c93b14612c0542528b071741f6eac4d71c1cc2
damonlemmons <jumproper22@gmail.com>
2018-12-03 15:07:21 -0800

    checking username

--- JSReviewday.js
+++ JSReviewday.js
@@ -25,33 +25,42 @@
 // palindrome("racecar")
 
 //Write a function that takes in two or more arrays (of numbers) and checks for duplicates.  The function should output a new array with only the unique numbers.
+//
+// var a = [1, 2, 3, 5, 10];
+// var b = [2, 30, 1, 17];
+//
+// // create function that takes in two arrays and returns a new array with no duplicates
+//
+// function dedupe(arr1, arr2){
+//     //create an empty array to push numbers into
+//     var emptyArr = []
+//     //concat the two arrays
+//     var combinedArr = arr1.concat(arr2)
+//     // console.log(combinedArr);
+//
+//     //compare the concatenated array to the empty array for duplciated
+//     for(let i = 0; i < combinedArr.length; i++){
+//         if(!emptyArr.includes(combinedArr[i])) {
+//     //push if not included in empty array
+//             emptyArr.push(combinedArr[i])
+//         }
+//     }
+//     return emptyArr
+// }
+//
+// console.log(dedupe(a, b))
 
-var a = [1, 2, 3, 5, 10];
-var b = [2, 30, 1, 17];
-
-// create function that takes in two arrays and returns a new array with no duplicates
-
-function dedupe(arr1, arr2){
-    //create an empty array to push numbers into
-    var emptyArr = []
-    //concat the two arrays
-    var combinedArr = arr1.concat(arr2)
-    // console.log(combinedArr);
-
-    //compare the concatenated array to the empty array for duplciated
-    for(let i = 0; i < combinedArr.length; i++){
-        if(!emptyArr.includes(combinedArr[i])) {
-    //push if not included in empty array
-            emptyArr.push(combinedArr[i])
-        }
-    }
-    return emptyArr
-}
-
-console.log(dedupe(a, b))
 
 
 
+
+var sentence = "The dog jumped over the fence"
+
+var split = sentence.split("")
+
+console.log(split);
+
+console.log(split[3])
 
 
 
